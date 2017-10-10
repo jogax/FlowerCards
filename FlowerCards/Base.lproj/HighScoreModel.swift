e6d5f4a1bd2eb7e05fe6f56520d2e59b0f51267d
jogax <jozsef.romhanyi@gmail.com>
2017-10-10 12:47:32 +0200

    New Model

---
+++ FlowerCards/Base.lproj/HighScoreModel.swift
@@ -0,0 +1,27 @@
+//
+//  HighScoreModel.swift
+//  FlowerCards
+//
+//  Created by Jozsef Romhanyi on 10/10/2017.
+//Copyright © 2017 Jozsef Romhanyi. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class HighScoreModel: Object {
+    @objc dynamic var ID = 0
+    @objc dynamic var levelID = 0
+    @objc dynamic var countPackages = 0
+    @objc dynamic var myHighScore = 0
+    @objc dynamic var sentToGameCenter = false
+    @objc dynamic var bestPlayerName = ""
+    @objc dynamic var bestPlayerHighScore = 0
+    @objc dynamic var created = Date()
+    
+    override  class func primaryKey() -> String {
+        return "ID"
+    }
+
+}
+
